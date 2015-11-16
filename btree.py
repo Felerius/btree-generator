6e40d5a68254eac3394f195ecfdb0fe573a3c422
David Stangl <david.stangl@student.hpi.de>
2015-11-16 18:11:58 +0100

    Rename num_keys to keys_per_block

--- btree.py
+++ btree.py
@@ -60,8 +60,8 @@ def fill_to_length(item_list, length, fill_item):
 
 def generate_graph(data):
     """Generates a graph in dot language from input data."""
-    num_keys = data.get('num_keys')
-    root_data = generate_node_graph(data, '0', num_keys)
+    keys_per_block = data.get('keys_per_block')
+    root_data = generate_node_graph(data, '0', keys_per_block)
     nodes = indent('\n'.join(root_data.generated_nodes), NODES_INDENT)
     connections = indent(
         '\n'.join(root_data.generated_connections),
@@ -73,20 +73,20 @@ def generate_graph(data):
     )
 
 
-def generate_node_graph(node_data, name, num_keys):
+def generate_node_graph(node_data, name, keys_per_block):
     """Generate NodeData for a single node and its descendents."""
     if isinstance(node_data, list):
         node_data = {'keys': node_data}
     if 'keys' not in node_data:
         return NodeData(name, None, [], [])
-    keys = fill_to_length(node_data['keys'], num_keys, DUMMY_KEY)
-    middle_connector, cells = generate_cells(keys, num_keys)
+    keys = fill_to_length(node_data['keys'], keys_per_block, DUMMY_KEY)
+    middle_connector, cells = generate_cells(keys, keys_per_block)
     cells_str = indent('\n'.join(cells), CELL_INDENT)
     node = NODE_TEMPLATE.format(name=name, cells=cells_str)
     sub_nodes = []
     if 'children' in node_data:
         sub_nodes = [
-            generate_node_graph(child, name + '.' + str(i), num_keys)
+            generate_node_graph(child, name + '.' + str(i), keys_per_block)
             for i, child in enumerate(node_data['children'])
         ]
     connections = [
@@ -109,24 +109,24 @@ def generate_node_graph(node_data, name, num_keys):
     return NodeData(name, middle_connector, all_nodes, all_connections)
 
 
-def generate_cells(keys, num_keys):
+def generate_cells(keys, keys_per_block):
     """Generate cells for a single block from it's keys.
 
-    Argument num_keys is only there, so that we can avoid creating
+    Argument keys_per_block is only there, so that we can avoid creating
     lists of keys and instead keep them generators.
     """
-    needs_middle = (num_keys % 2 == 1)
+    needs_middle = (keys_per_block % 2 == 1)
     cells = []
     for i, key in enumerate(keys):
         cells.append(CONNECTOR_TEMPLATE.format(number=i))
-        if needs_middle and i == (num_keys // 2):
+        if needs_middle and i == (keys_per_block // 2):
             cells.append(CELL_MIDDLE_TEMPLATE.format(content=key))
         else:
             cells.append(CELL_TEMPLATE.format(content=key))
-    cells.append(CONNECTOR_TEMPLATE.format(number=num_keys))
+    cells.append(CONNECTOR_TEMPLATE.format(number=keys_per_block))
     if needs_middle:
         return 'middle', cells
-    return CONNECTOR_NAME_TEMPLATE.format(number=num_keys // 2), cells
+    return CONNECTOR_NAME_TEMPLATE.format(number=keys_per_block // 2), cells
 
 
 def main():
